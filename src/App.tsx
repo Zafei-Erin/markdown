97d39f50498232e6f5a9ddd88ade12229fd638f8
Zafei-Erin <897659752@qq.com>
2023-02-19 12:28:06 +0800

    feat: edit and detele note

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { v4 as uuidV4 } from "uuid"
 import { NoteList } from "./note-list"
 import { NoteLayout } from "./NoteLayout"
 import { Note } from "./Note"
+import { EditNote } from "./EditNote"
 
 export type Note = {
   id: string;
@@ -57,6 +58,26 @@ function App() {
     })
   }
 
+  // 修改note，直接修改rawnote[]
+  function onUpdateNote(id:string, {tags, ...data}: NoteData) {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if (note.id === id) {
+          return {...note, ...data, tagIds: tags.map(tag => tag.id)}
+        } else {
+          return note
+        }
+      })
+    })
+  }
+
+  // 删除note
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   // 把tag存进tag的localstorage里
   function addTag(tag: Tag) {
     setTags(prev => [...prev, tag])
@@ -76,8 +97,15 @@ function App() {
               onAddTag={addTag}
               availableTags={tags}/>} />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags}/>}>
-          <Route index element={<Note/>}/>
-          <Route path="edit" element={<h1>Edit</h1>}/>
+          <Route index element={<Note onDelete={onDeleteNote}/>}/>
+          <Route 
+            path="edit" 
+            element={
+              <EditNote
+                onSubmit={onUpdateNote} 
+                onAddTag={addTag}
+                availableTags={tags}
+              />}/>
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
